06e41bd7f03da493c9118e68aa03a9d1e5283c28
unknown <Ing-Harold>
2021-06-21 00:23:12 -0400

    tu papi

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>HELLO GRUPO</title>
+</head>
+<body>
+    <H1> hola Darwin </H1>
+</body>
+</html>
